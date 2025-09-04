09cc9eb997eefc01854557acf15d9a4850a01624
Alous12 <alejandro.machaca@ucb.edu.bo>
2025-09-04 08:08:42 -0400

    Refactor: deshacerce de varios condicionalesy aumentar por un criterio

--- src/tennisScore.js
+++ src/tennisScore.js
@@ -3,54 +3,21 @@ class TennisScorer {
     this.player1Points = 0;
     this.player2Points = 0;
   }
-    showScore() 
-    {
-        if (this.player1Points === 1) 
-        {
-            return "15 - Love";
-        }
-        else
-        {
-            if (this.player1Points === 2)
-            {
-                return "30 - Love";
-            }
-            else
-            {
-                if (this.player1Points === 3)
-                {
-                    return "40 - Love";
-                }
-            }
-        }
 
-        if (this.player2Points === 1)
-        {
-            return "Love - 15";
-        }
-        else
-        {
-            if (this.player2Points === 2)
-            {
-                return "Love - 30";
-            }
-            else
-            {
-                if (this.player2Points === 3)
-                {
-                    return "Love - 40";
-                }
-            }
-        }
-            return "Love - Love";
-    }
-    player1Scores() 
-    {
-        this.player1Points++;
-    }
-    player2Scores() 
-    {
-        this.player2Points++;
-    }
+  showScore() {
+    const scoreNames = ["Love", "15", "30", "40"];
+    const p1 = scoreNames[this.player1Points] ;
+    const p2 = scoreNames[this.player2Points] ;
+    return `${p1} - ${p2}`;
+  }
+
+  player1Scores() {
+    this.player1Points++;
+  }
+
+  player2Scores() {
+    this.player2Points++;
+  }
 }
+
 export default TennisScorer;
